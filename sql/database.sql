23c3ff228c240c3cd854d32272ff4db13432258d
Brian Hache <brnhache@gmail.com>
2022-06-27 23:57:03 +0000

    Fixed the posts.id data type in sql script

--- sql/database.sql
+++ sql/database.sql
@@ -3,7 +3,7 @@ CREATE DATABASE blog_database;
 -- \c blog_database
 
 CREATE TABLE posts(
-    id PRIMARY KEY,
+    id INTEGER PRIMARY KEY,
     title TEXT NOT NULL, 
     body TEXT NOT NULL, 
     image TEXT NOT NULL
